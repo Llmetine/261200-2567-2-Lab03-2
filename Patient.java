5383cd9375334eb80aec0512f17cd7faa2fd641a
Jon <ntjeddelog@gmail.com>
2025-12-19 16:16:11 +0700

    Create Patient.java

---
+++ Patient.java
@@ -0,0 +1,96 @@
+public class Patient {
+    private String name,bloodGroup,phoneNumber;
+    private int id,birthYear;
+    private double height,weight;
+
+    Patient(int id,String name,int birthYear,double height,double weight) {
+        this.id = id;
+        this.name = name;
+        this.birthYear = birthYear;
+//        this.height=height;
+//        this.weight=weight;
+        this.phoneNumber = phoneNumber;
+        if(bloodGroup!="A"||bloodGroup!="B"||bloodGroup!="AB"||bloodGroup!="O"){
+            bloodGroup="Error";
+        }
+        else{
+            this.bloodGroup = bloodGroup;
+        }
+        if (height < 0||height >400) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+        if (weight < 0||weight>300) {
+            this.weight = 0;
+        } else {
+            this.weight = weight;
+        }
+    }
+
+    public int getid (){
+        if(id<=0) {
+            System.out.println("Id Error!!!");
+            return 0;}
+        else{ return this.id;}
+    }
+
+    public String getname(){
+        return  this.name;
+    }
+
+    public int getbirthYear(){
+        if(birthYear<=1800){
+            System.out.println("birthYear Error!!!");
+        return 0;
+        }
+        else{
+        return this.birthYear;}
+    }
+
+    public double getheight(){
+        if(height == 0){
+            System.out.println("Height Error!!!");}
+        return this.height;}
+    public double weight(){
+        if(weight == 0){
+            System.out.println("Weight Error!!!");}
+        return this.weight;
+    }
+
+    public int getAge(int currentYear){
+        int age;
+        age = currentYear-this.birthYear;
+        if(age<0){age=0;
+            System.out.println("birthYear Error!!!");}
+        return age;
+    }
+
+    public String getBloodGroup(){
+        return this.bloodGroup;
+    }
+
+    public String phoneNumber(){
+        return this.phoneNumber;
+    }
+
+    public double getBMI(){
+        this.height=height/100;
+        return this.weight/(this.height*this.height);
+    }
+
+    public void displayDetails(int currentYear) {
+        System.out.println("Patient Name: " + name);
+        System.out.println("Patient Age: " + getAge(currentYear));
+        System.out.println("Patient Height (cm): " + height);
+        System.out.println("Patient Weight (kg): " + weight);
+        System.out.println("BMI= "+getBMI());
+        if(height==0){
+            System.out.println("Height Error!!!");
+        }
+        if(weight==0){
+            System.out.println("Weight Error!!!");
+        }
+    }
+
+}
